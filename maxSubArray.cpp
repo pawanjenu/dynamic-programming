776eb714658dff920bbbddf3a5264946436d2c6a
pawanjenu <pawanjenu@gmail.com>
2019-06-30 22:36:59 +0530

    max sub array

---
+++ maxSubArray.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+ int maxSubArray(vector<int>& nums) {
+     int n = nums.size();
+     vector<long long> dp(n+1,0);
+     long long ans = -1234,prevmax = -5432,currmax;
+
+     for(int i = 0; i < n; i++){
+        currmax = nums[i];
+        currmax = max(currmax,nums[i] + prevmax);
+        ans = max(currmax,ans);
+        prevmax = currmax;
+
+     }
+
+     return ans;
+
+        
+    }
+
+int main(){
+    vector<int> t = {-2,1,-3,4,-1,2,1,-5,4};
+    cout <<maxSubArray(t);
+
+
+    return 0;
+}
